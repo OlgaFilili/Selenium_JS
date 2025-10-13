8cb29fcaf0c5827e78b278eb35d901346b2a974d
OlgaFilili <philippova.ov@gmail.com>
2025-10-13 23:45:17 +0300

    feat: add createDriver and quitDriver in driver.js

--- utils/driver.js
+++ utils/driver.js
@@ -1,4 +1,29 @@
 const { Builder } = require("selenium-webdriver");
+const chrome = require('selenium-webdriver/chrome');
 
-const driver= new Builder().forBrowser("chrome").build();
-module.exports= driver;
+let driver; // will be created once
+
+async function createDriver() {
+  if (!driver) {
+    const options = new chrome.Options();
+    console.log('>>> createDriver() started');
+    driver = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(options)
+      .build();
+    console.log('>>> createDriver() finished');
+  }
+
+  // Ensure the promise resolves before continuing
+  await driver.getSession();
+  return driver;
+}
+
+async function quitDriver() {
+  if (driver) {
+    await driver.quit();
+    driver = null;
+  }
+}
+
+module.exports = { createDriver, quitDriver };
