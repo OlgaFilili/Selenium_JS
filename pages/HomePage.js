6ba03df6b81032c6c3bd40636c6aa74fa0d19993
OlgaFilili <philippova.ov@gmail.com>
2025-10-13 23:43:25 +0300

    feat: update BasePage and HomePage according to the POM

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,11 +1,59 @@
+const BasePage = require("./BasePage");
+const ElementsPage = require('./ElementsPage.js');
+const FormsPage = require('./FormsPage.js');
+const AlertsFrameWindowsPage = require('./AlertsFrameWindowsPage');
+const WidgetsPage = require('./WidgetsPage');
+const InteractionsPage = require('./InteractionsPage');
+const BookStoreApplicationPage = require('./BookStoreApplicationPage');
+
 class HomePage extends BasePage 
 {
-  constructor() {
-    super();
-    this.url = 'https://demoqa.com/';
+  constructor(driver) {
+    super(driver);
+    this.ElementsCard= { xpath: "//div[@id='app']//h5[text()='Elements']"};
+    this.FormsCard= { xpath: "//div[@id='app']//h5[text()='Forms']"};
+    this.AlertsFrameWindowsCard= { xpath: "//div[@id='app']//h5[text()='Alerts, Frame & Windows']"};
+    this.WidgetsCard= { xpath: "//div[@id='app']//h5[text()='Widgets']"};
+    this.InteractionsCard= { xpath: "//div[@id='app']//h5[text()='Interactions']"};
+    this.BookStoreApplicationCard= { xpath: "//div[@id='app']//h5[text()='Book Store Application']"};
+  }
+ 
+  async gotoElements() {
+    await this._find(this.ElementsCard);
+    await this._click(this.ElementsCard);
+    return new ElementsPage(this.driver);
   }
 
-  async open() {
-    await this._open(this.url);
+  async gotoForms() {
+    await this._find(this.FormsCard);
+    await this._click(this.FormsCard);
+    return new FormsPage(this.driver);
   }
+
+  async gotoAlertsFrameWindows() {
+    await this._find(this.AlertsFrameWindowsCard);
+    await this._click(this.AlertsFrameWindowsCard);
+    return new AlertsFrameWindowsPage(this.driver);
+  }
+
+  async gotoWidgets() {
+    await this._find(this.WidgetsCard);
+    await this._click(this.WidgetsCard);
+    return new WidgetsPage(this.driver);
+  }
+
+  async gotoInteractions() {
+    await this._find(this.InteractionsCard);
+    await this._click(this.InteractionsCard);
+    return new InteractionsPage(this.driver);
+  }
+
+  async gotoBookStoreApplication() {
+    await this._find(this.BookStoreApplicationCard);
+    await this._click(this.BookStoreApplicationCard);
+    return new BookStoreApplicationPage(this.driver);
+  }
+
 }
+
+module.exports = HomePage;
