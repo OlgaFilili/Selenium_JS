8752ba9184ef8219e968423792ee87bebeb3b260
OlgaFilili <philippova.ov@gmail.com>
2026-01-10 20:10:47 +0300

    feature: add authentication UI and API flows for Book Store

--- pages/book_store/BooksPage.js
+++ pages/book_store/BooksPage.js
@@ -1,25 +1,26 @@
-const BasePage = require("../BasePage.js");
+const AuthenticatedPage = require("./AuthenticatedPage.js");
 const MainMenu = require("../../components/MainMenu.js");
+const LoginPage = require("../book_store/LoginPage.js");
 const { waitVisible } = require("../../utils/WaitUtils.js");
 const { isInViewport } = require("../../utils/BrowserUtils.js");
 
-class BooksPage extends BasePage 
+class BooksPage extends AuthenticatedPage
 {
     constructor(driver) {
         super(driver);
         this.menu= new MainMenu(driver);
-        this.loginButton= { id: "login"};
+        this.loginButton= { id: "login" };
     }
-    async waitPageReady(){
-        await waitVisible(this.driver, this.loginButton);
-    }
+
     async clickLoginButton(){
-        await this.waitPageReady();
+        await waitVisible(this.driver, this.loginButton);
         await this._click(this.loginButton);
+        return new LoginPage(this.driver);
     }
     async isLoginButtonVisible(){
         const element= await waitVisible(this.driver, this.loginButton);
         return await isInViewport(this.driver, element);
     }
+
 }
 module.exports = BooksPage;
